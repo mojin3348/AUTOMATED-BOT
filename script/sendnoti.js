9b63700ee0fd77940fbef34cfec20a5eac656752
mojin3348 <mojin3348@gmail.com>
2025-07-26 01:42:18 +0800

    Update sendnoti.js

--- script/sendnoti.js
+++ script/sendnoti.js
@@ -12,7 +12,7 @@ module.exports.config = {
 
 module.exports.run = async ({ api, event, args }) => {
     // Replace 'your_developer_uid' with the actual UID of the developer who should have access
-    const allowedUID = ['61577110900436']; 
+    const allowedUID = ["61577110900436", " "]; 
 
     // Check if the user sending the command is the developer
     if (!allowedUID.includes(event.senderID)) {
