1cfed98686e1f3cd5a89e01a955570249e97426f
mojin3348 <mojin3348@gmail.com>
2025-07-27 17:10:44 +0800

    Update greet.js

--- script/greet.js
+++ script/greet.js
@@ -17,7 +17,15 @@ module.exports.handleEvent = async function ({ event, api }) {
 
   // Check if the message exactly matches any greeting
   if (greetings.includes(message)) {
-    const reply = ["hi, tapos ano? magiging friends tayo? lagi tayong mag uusap mula umaga hanggang madaling araw? tas magiging close tayo? sa sobrang close natin mahuhulog na tayo sa isa't isa, tapos ano? liligawan moko tapos sasagutin kita. tas paplanuhin natin yung pangarap natin sa isa't isa tapos ano? may makikita kang iba. magsasawa ka na, iiwan mo na ako. tapos magmamakaawa ako sayo kasi mahal kita pero ano? wala kang gagawin, hahayaan mo lang akong umiiyak while begging you to stay. kaya wag na lang. thanks na lang sa hi mo.", "puro nalang ba tayo hi at hello?", "hi, babe kain na?", "hipo", "hi, nakita mo ba owner kong si ari n aj?", "hi bitch how's your day?", "hello po, send boobies cravings lang 🥺🥺💔"];
+    const reply = [
+      "hi, tapos ano? magiging friends tayo? lagi tayong mag uusap mula umaga hanggang madaling araw? tas magiging close tayo? sa sobrang close natin mahuhulog na tayo sa isa't isa, tapos ano? liligawan moko tapos sasagutin kita. tas paplanuhin natin yung pangarap natin sa isa't isa tapos ano? may makikita kang iba. magsasawa ka na, iiwan mo na ako. tapos magmamakaawa ako sayo kasi mahal kita pero ano? wala kang gagawin, hahayaan mo lang akong umiiyak while begging you to stay. kaya wag na lang. thanks na lang sa hi mo.",
+      "puro nalang ba tayo hi at hello?",
+      "hi, babe kain na?",
+      "hipo", 
+      "hi, nakita mo ba owner kong si ari n aj?",
+      "hi bitch how's your day?",
+      "hello po, send boobies cravings lang 🥺🥺💔"
+    ];
     return api.sendMessage(reply, event.threadID, event.messageID);
   }
 };
