22f3e7cf8c03000346f75a4096adeee3b046559f
mojin3348 <mojin3348@gmail.com>
2025-07-27 21:59:56 +0800

    Update greet.js

--- script/greet.js
+++ script/greet.js
@@ -24,11 +24,11 @@ module.exports.handleEvent = async function ({ event, api }) {
   if (greetings.includes(message)) {
     const replies = [
       "hi, tapos ano? magiging friends tayo? lagi tayong mag uusap mula umaga hanggang madaling araw? tas magiging close tayo? sa sobrang close natin mahuhulog na tayo sa isa't isa, tapos ano? liligawan moko tapos sasagutin kita. tas paplanuhin natin yung pangarap natin sa isa't isa tapos ano? may makikita kang iba. magsasawa ka na, iiwan mo na ako. tapos magmamakaawa ako sayo kasi mahal kita pero ano? wala kang gagawin, hahayaan mo lang akong umiiyak while begging you to stay. kaya wag na lang. thanks na lang sa hi mo.",
-      "hipo",
+      "ano na naman ba? panay ka hi at hello",
       "puro nalang ba tayo hi at hello?",
       "hi babe, kain?",
       "hi, nakita mo ba owner kong si ari n aj?",
-      "hi bitc, how's your day?",
+      "hi bitch, how's your day?",
       "hi po, send boobies cravings lang 🥺🥺💔",
       "👋 Kumusta ka?",
       "Hello hello! 🔔",
