1d0f8349ae8de53493b693950f9eb3e290cd8d54
mojin3348 <mojin3348@gmail.com>
2025-08-02 16:48:58 +0800

    Update Welcome.js

--- script/event/Welcome.js
+++ script/event/Welcome.js
@@ -1,80 +1,45 @@
-const fs = require("fs");
-const axios = require("axios");
-const moment = require("moment"); 
+const axios = require('axios');
+const fs = require('fs');
 
 module.exports.config = {
     name: "welcome",
-    eventType: ['log:subscribe'],
     version: "1.0.0",
-    credits: "Mirai-Team", // mod by Jonell Magallanes 
-    description: "GROUP UPDATE NOTIFICATION"
 };
 
-module.exports.run = async function ({ api, event, Users, Threads }) {
-    function reply(data) {
-        api.sendMessage(data, event.threadID, event.messageID);
-    }
+module.exports.handleEvent = async function ({ api, event }) {
+    if (event.logMessageType === "log:subscribe") {
+        const addedParticipants = event.logMessageData.addedParticipants;
+        const senderID = addedParticipants[0].userFbId;
+        let name = await api.getUserInfo(senderID).then(info => info[senderID].name);
 
-    if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
-        api.changeNickname(`${global.config.BOTNAME} • [ ${global.config.PREFIX} ]`, event.threadID, api.getCurrentUserID());
-        return reply(`✅ | ${global.config.BOTNAME} connected successfully!\nType "${global.config.PREFIX}help" to view all commands\n\nContact the admin if you encounter an error.\n\n👷Developer: [Ari]`);
-    } else {
+        // Truncate name if it's too long
+        const maxLength = 15;
+        if (name.length > maxLength) {
+            name = name.substring(0, maxLength - 3) + '...';
+        }
+
+        const groupInfo = await api.getThreadInfo(event.threadID);
+        const groupIcon = groupInfo.imageSrc || "https://i.ibb.co/G5mJZxs/rin.jpg";
+        const memberCount = groupInfo.participantIDs.length;
+        const groupName = groupInfo.threadName || "this group";
+        const background = groupInfo.imageSrc || "https://i.ibb.co/4YBNyvP/images-76.jpg";
+
+        const url = `https://hershey-api.onrender.com/api/welcome?username=${encodeURIComponent(name)}&avatarUrl=https://api-canvass.vercel.app/profile?uid=${senderID}&groupname=${encodeURIComponent(groupName)}&bg=${encodeURIComponent(background)}&memberCount=${memberCount}`;
+
         try {
-            const {
-                threadID
-            } = event;
-            let {
-                threadName,
-                participantIDs
-            } = await api.getThreadInfo(threadID);
-            var tn = threadName || "Unnamed group";
-            var mentions = [],
-                nameArray = [],
-                i = 0;
-            let addedParticipants1 = event.logMessageData.addedParticipants;
+            const { data } = await axios.get(url, { responseType: 'arraybuffer' });
+            const filePath = './script/cache/welcome_image.jpg';
+            fs.writeFileSync(filePath, Buffer.from(data));
 
-            for (let newParticipant of addedParticipants1) {
-                let userID = newParticipant.userFbId;
-                let senderID = newParticipant.userFbId; 
-                api.getUserInfo(parseInt(userID), (err, data) => {
-                    if (err) {
-                        return console.log(err);
-                    }
-                    var obj = Object.keys(data);
-                    var firstName = data[obj].firstName;
-                    var userName = data[obj].name.replace("@", "");
-
-                    if (userID !== api.getCurrentUserID()) {
-                        nameArray.push(userName);
-                        mentions.push({
-                            tag: userName,
-                            id: userID,
-                            fromIndex: 0
-                        });
-                        let avt = ["https://i.postimg.cc/pTGHDKnY/images-2023-08-19-T230758-444.jpg", "https://i.postimg.cc/pd0WBwwF/images-2023-08-19-T230807-555.jpg", "https://i.postimg.cc/gkvG7L9d/images-2023-08-19-T230828-578.jpg", "https://i.postimg.cc/XNfXtYyf/images-2023-08-19-T230845-301.jpg"];
-                        var avt1 = avt[Math.floor(Math.random() * avt.length)];
-
-                        let firstName = nameArray[0].split(" ")[0];
-
-                        let requestURL = `https://join2apibyjonell-7b4fde8396f3.herokuapp.com/join2?name=${firstName}&id=${senderID}&background=${avt1}&count=${participantIDs.length}`;
-
-                        axios.get(encodeURI(requestURL), { responseType: 'arraybuffer' })
-                            .then(response => {
-                                fs.writeFileSync(`come.jpg`, Buffer.from(response.data, 'binary'));
-                                let welcomeText = `Hello ${userName}!\nWelcome to ${tn}\nYou're the ${participantIDs.length}th member on this group. Enjoy!`;
-
-                                return reply({
-                                    body: welcomeText,
-                                    attachment: fs.createReadStream(`come.jpg`),
-                                    mentions
-                                }, () => fs.unlinkSync(`come.jpg`));
-                            })
-                            .catch(error => console.log("Axios Error: ", error));
-                    }
-                })
-            }
-        } catch (err) {
-            return console.log("ERROR: " + err);
+            api.sendMessage({
+                body: `Everyone welcome the new member ${name} to ${groupName}!`,
+                attachment: fs.createReadStream(filePath)
+            }, event.threadID, () => fs.unlinkSync(filePath));
+        } catch (error) {
+            console.error("Error fetching welcome image:", error);
+            api.sendMessage({
+                body: `🤖 | Everyone welcome the new member ${name} to ${groupName}!`
+            }, event.threadID);
         }
     }
 };
